f1f305292ea74255a01517cc14eef294c306ae81
Emily Marigold Klassen <forivall@gmail.com>
2018-05-03 12:26:01 -0700

    fix: improve type definitions and export default

--- lib/mock-server.d.ts
+++ lib/mock-server.d.ts
@@ -2,43 +2,39 @@
 import {JsonSchema} from 'tv4';
 import {Application} from 'express';
 
+export default function load(def: load.ExtractedApi, callback: Function);
+export function createApp(options: CreateAppOptions): Application;
+export function extract(schema: JsonSchema): {resources: Resources, definitions: Definitions};
+
 type OneOrMany<T> = T | T[];
 type JsonValue = OneOrMany<string | number | boolean | null | {[key: string]: JsonValue}>;
 
-declare function load(def: load.ExtractedApi, callback: Function);
-
-declare namespace load {
-  interface Resources {
-    [route: string]: {
-      [method: string]: {
-        [status: string]: {
-          _ref: string;
-          schema: JsonSchema;
-          example: JsonValue;
-        }
+interface Resources {
+  [route: string]: {
+    [method: string]: {
+      [status: string]: {
+        _ref: string;
+        schema: JsonSchema;
+        example: JsonValue;
       }
     }
   }
-  interface Definitions {
-    [ref: string]: JsonSchema;
-  }
-  interface ExtractedApi {
-    resources: Resources;
-  }
-  interface CreateAppOptions {
-    api: ExtractedApi;
-    forceExample?: boolean;
-    hooks?: {
-      [route: string]: {
-        [method: string]: (sample: any, req: Request, res: Response) => any;
-      }
-    }
-    refs?: any;
-    statuses?: (number | string)[];
-    log: (...message: string[]) => any;
-  }
-  function createApp(options: CreateAppOptions): Application;
-  function extract(schema: JsonSchema): {resources: Resources, definitions: Definitions};
 }
-
-export = load;
+interface Definitions {
+  [ref: string]: JsonSchema;
+}
+interface ExtractedApi {
+  resources: Resources;
+}
+interface CreateAppOptions {
+  api: ExtractedApi;
+  forceExample?: boolean;
+  hooks?: {
+    [route: string]: {
+      [method: string]: (sample: any, req: Request, res: Response) => any;
+    }
+  }
+  refs?: any;
+  statuses?: (number | string)[];
+  log: (...message: string[]) => any;
+}
